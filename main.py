a5b23a9c7c67add59f7c219d94b0b20abf5e83b5
SmileyBoy321 <Ungurkristen@gmail.com>
2020-08-30 23:13:50 +0300

    feels_one_click_man

--- main.py
+++ main.py
@@ -1,116 +1,56 @@
 # This Python file uses the following encoding: utf-8
-import youtube_dl
 import sys
 import os
+from PySide2.QtWidgets import QApplication, QSystemTrayIcon, QFileDialog
+from PySide2.QtGui import QIcon
+from PySide2.QtCore import QCoreApplication
+import youtube_dl
+import pyperclip
+import Resources
 
 
-from PySide2.QtWidgets import (QApplication, QWidget,
-                               QPushButton, QLineEdit,
-                               QFileDialog, QLabel,
-                               QProgressBar, QCheckBox
-                               )
-from PySide2.QtCore import QFile, QThread, Signal
-from PySide2.QtUiTools import QUiLoader
-from PySide2 import QtXml
-from PySide2.QtWinExtras import QtWin
-from PySide2 import QtGui
-import Resource
-
-myappid = "com.nocompany.YouLoader"
-QtWin.setCurrentProcessExplicitAppUserModelID(myappid)
-
-
-class GUI(QWidget):
+class SystemTrayIcon:
     def __init__(self):
-        super(GUI, self).__init__()
-        self.load_ui()
-        self.setFixedSize(607, 154)
-
-        self.dirPath = self.findChild(QLineEdit, "lineEditDir")
-        self.downloadURL = self.findChild(QLineEdit, "lineEditURL")
-        self.browse = self.findChild(QPushButton, "pushButtonDir")
-        self.download = self.findChild(QPushButton, "pushButtonDownload")
-        self.progressText = self.findChild(QLabel, "lblProgressText")
-        self.progressBar = self.findChild(QProgressBar, "progressBar")
-        self.playlistCB = self.findChild(QCheckBox, "PlaylistCheckbox")
-
-        self.worker = Worker(self)
-        self.browse.clicked.connect(self.onBrowseClicked)
-        self.worker.updateProgress.connect(self.setProgress)
-        self.worker.updateText.connect(self.setText)
-        self.download.clicked.connect(self.worker.start)
-        self.playlistCB.clicked.connect(self.on_off)
-
-        self.playlist = None
-
-    def load_ui(self):
-        loader = QUiLoader()
-        path = os.path.join(os.path.dirname(__file__), "form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
-        ui_file.close()
-
-    def onBrowseClicked(self):
-        filename = QFileDialog.getExistingDirectory()
-
-        if filename:
-            self.dirPath.setText(filename)
-            os.chdir(self.dirPath.text())
-
-    def setProgress(self, progress):
-        self.progressBar.setValue(progress)
-
-    def setText(self, str):
-        self.progressText.setText(str)
-
-    def on_off(self):
-        if self.playlistCB.isChecked():
-            self.playlist = False
-        else:
-            self.playlist = True
-
-
-class Worker(QThread):
-    updateProgress = Signal(float)
-    updateText = Signal(str)
-
-    def __init__(self, main_window):
-        self.main_window = main_window
-        super(Worker, self).__init__(main_window)
-
-    def run(self):
-        self.main_window.progressBar.setValue(0)
+        self.tray = QSystemTrayIcon(QIcon(":/icons/icon.ico"))
+        self.tray.show()
+        self.tray.activated.connect(self.right_or_left_click)
 
         self.ydl_opts = {
-            'format': 'bestaudio/best',
-            'noplaylist': self.main_window.playlist,
-            'source_address': '0.0.0.0',
-            'progress_hooks': [self.my_hook],
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3'
-            }]
+            "format": "bestaudio/best",
+            "noplaylist": True,
+            "source_address": "0.0.0.0",
+            "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}],
         }
 
-        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
-            ydl.download([f'{self.main_window.downloadURL.text()}'])
+    def right_or_left_click(self, reason):
+        copied_link = pyperclip.paste()
+        if reason == QSystemTrayIcon.ActivationReason.Trigger:
+            if "youtube.com" in copied_link:
+                filepath = QFileDialog.getExistingDirectory()
+                os.chdir(filepath)
+                with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+                    ydl.download([f"{copied_link}"])
+                    self.tray.showMessage(
+                        "Finished downloading the music",
+                        "Now converting the music to mp3",
+                        self.icon,
+                        msec=1000,
+                    )
+            else:
+                self.tray.showMessage(
+                    "Invalid link",
+                    "Did not find youtube.com in the link",
+                    self.icon,
+                    msec=1000,
+                )
+        elif reason == QSystemTrayIcon.ActivationReason.Context:
+            QCoreApplication.exit()
 
-    def my_hook(self, download):
-        if download["status"] == "finished":
-            print("Finished")
-            self.main_window.progressText.setText("Ready")
-            self.main_window.downloadURL.clear()
-        if download["status"] == "downloading":
-            self.updateText.emit("Downloading...")
-            percent = download["_percent_str"]
-            percent = percent.replace("%", "")
-            self.updateProgress.emit(float(percent))
+    def exitClicked(self):
+        QCoreApplication.exit()
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    app.setWindowIcon(QtGui.QIcon(":/icon/window_icon.ico"))
-    widget = GUI()
-    widget.show()
+    widget = SystemTrayIcon()
     sys.exit(app.exec_())
