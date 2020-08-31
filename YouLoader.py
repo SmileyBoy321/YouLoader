3f64f5f9727f05489d7e19d7177b6e3d0faf31ec
SmileyBoy321 <Ungurkristen@gmail.com>
2020-08-31 18:16:42 +0300

    removed toast because pyinstaller doesn't like it

--- YouLoader.py
+++ YouLoader.py
@@ -1,35 +1,24 @@
-from win10toast import ToastNotifier
 from tkinter import filedialog
 import tkinter as tk
 import youtube_dl
 import pyperclip
 import os
+
 root = tk.Tk()
 root.withdraw()
-toaster = ToastNotifier()
 
 
 ydl_opts = {
-    'format': 'bestaudio/best',
-    'noplaylist': True,
-    'source_address': '0.0.0.0',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3'
-    }]
+    "format": "bestaudio/best",
+    "noplaylist": True,
+    "source_address": "0.0.0.0",
+    "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}],
 }
 
 copied_link = pyperclip.paste()
+
 if "youtube.com" in copied_link:
     file_path = filedialog.askdirectory()
     os.chdir(file_path)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([f'{copied_link}'])
-        toaster.show_toast("Finished downloading music",
-                           "Now converting the music to mp3",
-                           icon_path=r"C:\Users\SmileyBoy\Desktop\icon.ico")
-else:
-    toaster.show_toast("Invalid link",
-                       "Did not find youtube.com in the link",
-                       icon_path=r"C:\Users\SmileyBoy\Desktop\icon.ico")
-
+        ydl.download([f"{copied_link}"])
