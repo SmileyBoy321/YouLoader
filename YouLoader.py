674fd4206f67653ee99a890ab698c01a723ed3c2
SmileyBoy321 <Ungurkristen@gmail.com>
2020-09-10 19:54:19 +0300

    let error die quietly

--- YouLoader.py
+++ YouLoader.py
@@ -4,6 +4,7 @@ import tkinter as tk
 import youtube_dl
 import pyperclip
 import os
+import sys
 
 root = tk.Tk()
 root.withdraw()
@@ -20,13 +21,18 @@ copied_link = pyperclip.paste()
 iconPath = os.path.abspath(os.path.join(os.path.dirname(__file__), "icon.ico"))
 
 if "youtube.com" in copied_link:
-    file_path = filedialog.askdirectory()
-    os.chdir(file_path)
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([f"{copied_link}"])
-        toast.show_toast(
-            "Music downloaded", "Ready to download another one", duration=2, icon_path=iconPath
-        )
+	try:
+	    file_path = filedialog.askdirectory()
+	    os.chdir(file_path)
+	    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+	        ydl.download([f"{copied_link}"])
+	        toast.show_toast(
+	            "Music downloaded", "Ready to download another one", duration=2, icon_path=iconPath
+	        )
+	except OSError:
+		sys.exit(0) # die quietly snake and don't bother the user
+		# error happens when user doesn't choose a directory and clicks X
+		# button during the fileDirectory input
 else:
     toast.show_toast(
         "Invalid youtube link",
