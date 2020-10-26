20a6ab02ace315adf0ffcef55415021da5579e27
SmileyBoy321 <Ungurkristen@gmail.com>
2020-10-26 16:44:41 +0200

    FFMPEG console no longer pops up

--- YouLoader.py
+++ YouLoader.py
@@ -1,38 +1,45 @@
 from win10toast import ToastNotifier
-from tkinter import filedialog
-import tkinter as tk
-import youtube_dl
 import pyperclip
 import os
 import sys
+import subprocess
 
-root = tk.Tk()
-root.withdraw()
 toast = ToastNotifier()
-
-ydl_opts = {
-    "format": "bestaudio/best",
-    "noplaylist": True,
-    "source_address": "0.0.0.0",
-    "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}],
-}
 
 copied_link = pyperclip.paste()
 iconPath = os.path.abspath(os.path.join(os.path.dirname(__file__), "icon.ico"))
+desktop = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
+os.chdir(desktop)
 
 if "youtube.com" in copied_link:
-	try:
-	    file_path = filedialog.askdirectory()
-	    os.chdir(file_path)
-	    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	        ydl.download([f"{copied_link}"])
-	        toast.show_toast(
-	            "Music downloaded", "Ready to download another one", duration=2, icon_path=iconPath
-	        )
-	except OSError:
-		sys.exit(0) # die quietly snake and don't bother the user
-		# error happens when user doesn't choose a directory and clicks X
-		# button during the fileDirectory input
+    try:
+        if os.path.isdir("Music"):
+            os.chdir("Music")
+            subprocess.Popen(
+                ["youtube-dl", "--no-playlist", "-x", "--audio-format", "mp3", copied_link], shell=True
+            )
+            toast.show_toast(
+                "Music downloaded",
+                "Ready to download another one",
+                duration=2,
+                icon_path=iconPath,
+            )
+        else:
+            os.mkdir("Music")
+            os.chdir("Music")
+            subprocess.Popen(
+                ["youtube-dl", "--no-playlist", "-x", "--audio-format", "mp3", copied_link], shell=True
+            )
+            toast.show_toast(
+                "Music downloaded",
+                "Ready to download another one",
+                duration=2,
+                icon_path=iconPath,
+            )
+    except OSError:
+        sys.exit(0)  # Let python program die quietly and not bother the user
+        # error happens when user doesn't choose a directory and clicks X
+        # button during the fileDirectory input
 else:
     toast.show_toast(
         "Invalid youtube link",
